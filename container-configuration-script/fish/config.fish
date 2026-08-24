7a11858c96d50f97a43e64dc35bdc334ad2ca964
casualtyface <99969787+casualtyface@users.noreply.github.com>
2026-08-25 00:57:05 +0300

    fish del

--- container-configuration-script/fish/config.fish
+++ container-configuration-script/fish/config.fish
@@ -29,7 +29,6 @@ alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 alias cat='batcat'
-alias cfastfetch='clear && fastfetch'
 alias clear="clear && fastfetch"
 
 # Other
